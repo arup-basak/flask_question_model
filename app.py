ec395a86f910d8cad68f5e339b6a07d5ca863c7e
Arup Basak <email.arupbasak@gmail.com>
2023-02-03 12:15:53 +0530

    updated to numpy

--- app.py
+++ app.py
@@ -1,26 +1,25 @@
 from flask import Flask, render_template
 import json
+import numpy as np
 
 app = Flask(__name__)
 
 f = open('./local/data.json')
 json_arr = json.load(f)
 
-question_data = []
+question_data = np.array([])
 option_data = [[]]
-mcqs = []
 
 for obj in json_arr:
-    question_data.append(obj['question'])
+    mcqs = []
+    question_data = np.append(question_data, obj['question'])
     for opt in obj['mcq']:
         mcqs.append(opt['option'])
-    print(mcqs)
-    mcqs.clear()
+    option_data.append(mcqs)
 
-print(option_data)
-# @app.route("/")
-# def main_page():
-#     return render_template('index.html', len=len(json_arr), questions=question_data, mcqs=option_data)
+@app.route("/")
+def main_page():
+    return render_template('index.html', len=len(json_arr), questions=question_data, mcqs=option_data)
 
-# if __name__ == "__main__":
-#     app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
--- static/js/question_paper.js
+++ static/js/question_paper.js
@@ -1,1 +1,14 @@
-console.log(data)
+const options = document.querySelectorAll('.option')
+
+let current = 0
+const answers = Array.from({ length: options.length }).fill(-1);
+
+for (let i = 0; i < options.length; i++) {
+    options[i].addEventListener('click', () => {
+        answers[current] = i 
+    })
+}
+
+const changeQuestion = (questionNumber) => {
+    current = questionNumber
+}
