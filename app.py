78dbe231ff3fbfbc65eed7015a4d338838aacf87
Arup Basak <email.arupbasak@gmail.com>
2023-02-03 12:39:35 +0530

    update data  changed array to directly json

--- app.py
+++ app.py
@@ -7,19 +7,9 @@ app = Flask(__name__)
 f = open('./local/data.json')
 json_arr = json.load(f)
 
-question_data = np.array([])
-option_data = [[]]
-
-for obj in json_arr:
-    mcqs = []
-    question_data = np.append(question_data, obj['question'])
-    for opt in obj['mcq']:
-        mcqs.append(opt['option'])
-    option_data.append(mcqs)
-
 @app.route("/")
 def main_page():
-    return render_template('index.html', len=len(json_arr), questions=question_data, mcqs=option_data)
+    return render_template('index.html', len=len(json_arr), data=json_arr)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- static/js/question_paper.js
+++ static/js/question_paper.js
@@ -1,7 +1,10 @@
 const options = document.querySelectorAll('.option')
+const questionContainer = document.querySelectorAll('.question_container')
 
-let current = 0
+let current = -1
+
 const answers = Array.from({ length: options.length }).fill(-1);
+const question_state = Array.from({ length: options.length }).fill(-1); //nothing = -1, viewd = 0, marked - 1, for_later = 2
 
 for (let i = 0; i < options.length; i++) {
     options[i].addEventListener('click', () => {
@@ -9,6 +12,15 @@ for (let i = 0; i < options.length; i++) {
     })
 }
 
-const changeQuestion = (questionNumber) => {
+const setQuestion = (questionNumber) => {
+    if (current != -1) {
+        questionContainer[current].style.display = 'none'
+        if (question_state(questionNumber) == -1) {
+            question_state(0)
+        }
+    }
+    questionContainer[questionNumber].style.display = 'block'
     current = questionNumber
 }
+
+setQuestion(0)
