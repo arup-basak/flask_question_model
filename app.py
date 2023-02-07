74cd6563067c7d652fa6ee48944d062757ab4920
Arup Basak <email.arupbasak@gmail.com>
2023-02-07 20:35:12 +0530

    added match function, and added operated buttons

--- app.py
+++ app.py
@@ -7,9 +7,20 @@ app = Flask(__name__)
 f = open('./local/data.json')
 json_arr = json.load(f)
 
+
 @app.route("/")
 def main_page():
     return render_template('index.html', len=len(json_arr), data=json_arr)
+
+def match(answers):
+    correct = 0
+    arr = np.array(answers)
+    for index, answer in enumerate(arr):
+        answer_index = json_arr[index]['ans']
+        if (answer == json_arr[index]['mcq'][answer_index]['option']):
+            correct += 1
+    return correct
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- static/js/question_paper.js
+++ static/js/question_paper.js
@@ -1,26 +1,48 @@
 const options = document.querySelectorAll('.option')
 const questionContainer = document.querySelectorAll('.question_container')
 
+const previousButton = document.querySelector('#previous')
+const nextButton = document.querySelector('#next')
+
 let current = -1
+let totalQuestions = questionContainer.length;
 
 const answers = Array.from({ length: options.length }).fill(-1);
-const question_state = Array.from({ length: options.length }).fill(-1); //nothing = -1, viewd = 0, marked - 1, for_later = 2
+const question_state = Array.from({ length: options.length }).fill(-1); //nothing = -1, viewed = 0, marked = 1, for_later = 2
 
 for (let i = 0; i < options.length; i++) {
-    options[i].addEventListener('click', () => {
-        answers[current] = i 
-    })
+    options[i].addEventListener("click", () => {
+        answers[current] = i;
+    });
 }
 
 const setQuestion = (questionNumber) => {
     if (current != -1) {
         questionContainer[current].style.display = 'none'
-        if (question_state(questionNumber) == -1) {
-            question_state(0)
+        if (question_state[questionNumber] == -1) {
+            question_state[questionNumber] = 0
         }
     }
     questionContainer[questionNumber].style.display = 'block'
     current = questionNumber
+    if (current == 0) {
+        previousButton.disabled = true;
+    }
+    else if (current == totalQuestions-1) {
+        nextButton.disabled = true
+    }
+    else {
+        previousButton.disabled = false;
+        nextButton.disabled = false
+    }
 }
+
+previousButton.addEventListener('click', () => {
+    setQuestion(current -1)
+})
+
+nextButton.addEventListener('click', () => {
+    setQuestion(current + 1)
+})
 
 setQuestion(0)
